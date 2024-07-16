cf7f43e97aab6c47286dcca53e0f09cea7a9b451
Anton <offlineanton@hotmail.co.uk>
2024-07-16 09:58:27 +0100

    Install card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 function App() {
 
   return (
     <>
-        <h1 className="bg-red-50">
-          Hello world!
-        </h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </>
   )
 }
